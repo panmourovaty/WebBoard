0f0ea3fbb0f8a15a5148c3e50c997449bf1be6cb
panmourovaty <panmourovaty@centrum.cz>
2023-01-09 02:18:14 +0100

    fix title

--- craftboard/common.php
+++ craftboard/common.php
@@ -1,7 +1,7 @@
 <?php
 echo '
 <meta charset="utf-8">
-<title>minerboard</title>
+<title>CraftBoard</title>
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <meta content="" name="keywords">
 <meta content="" name="description">
