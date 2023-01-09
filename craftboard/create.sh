d87e2f4999d8e6ed02a501b83f0a32f84f22d0f0
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2023-01-09 02:29:20 +0100

    fix truepath parsing

--- craftboard/create.sh
+++ craftboard/create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $1/files/servers/$2
-TRUEPATH = cat ./files/truepath
+TRUEPATH=$(cat /var/www/app/files/truepath)
 FIXPATH="$TRUEPATH/servers/$2/server"
 sed -i -e "s/container_name.*/container_name: $2/g" docker-compose.yml
 sed -i -e "s/- 25565:25565/- $3:25565/g" docker-compose.yml
