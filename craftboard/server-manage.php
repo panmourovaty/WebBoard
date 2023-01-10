06ff5732750f57fdaefd66c1704fff491cc122ac
panmourovaty <panmourovaty@centrum.cz>
2023-01-10 05:21:25 +0100

    improve edit speed

--- craftboard/server-manage.php
+++ craftboard/server-manage.php
@@ -24,8 +24,7 @@ switch ($_GET["server_action"]) {
       $file = fopen('./files/servers/'.$_GET['server_name'].'/docker-compose.yml',"w");
       fwrite($file, $_POST['newtext']);
       fclose($file);
-      shell_exec('cd ./files/servers/'.$_GET["server_name"].' && docker-compose down && docker-compose up -d');
-      sleep(1);
+      shell_exec('cd ./files/servers/'.$_GET["server_name"].' && docker-compose stop && docker-compose up -d');
       echo "<script>window.close()</script>";
       break;
 }
