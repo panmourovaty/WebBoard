89b131aef4faa0f9e6bf312b7ba035c3f69c02ee
panmourovaty <panmourovaty@centrum.cz>
2023-01-10 02:16:27 +0100

    add inspect function

--- craftboard/runner-manage.php
+++ craftboard/runner-manage.php
@@ -1,8 +1,15 @@
 <?php
 switch ($_GET["runner_action"]) {
   case "update":
-      shell_exec('docker pull '.$_GET["runner_name"]);
-      header('Location: ' . $_SERVER['HTTP_REFERER']);
-      break;
+    shell_exec('docker pull '.$_GET["runner_name"]);
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    break;
+  case "inspect":
+    include 'common.php';
+    echo '<div class="bg-light h-100 p-4">';
+    echo "<title>".$_GET["runner_name"]." info</title>";
+    $output = nl2br(shell_exec('docker image inspect '.$_GET["runner_name"]));
+    echo "<p style=\"font-family:Heebo\">".$output."</p></div>";
+    break;
 }
 ?>
