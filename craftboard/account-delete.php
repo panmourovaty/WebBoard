73b87e81ca6c7246bcc0892a341a11f6b87e45e0
panmourovaty <panmourovaty@centrum.cz>
2023-01-11 11:59:22 +0100

    bugfix of delete button

--- craftboard/account-delete.php
+++ craftboard/account-delete.php
@@ -2,9 +2,9 @@
     require 'account-common.php';
     require 'database.php';
     require 'adminrestrict.php';
-    if ($_POST['username'] == "admin") {
+    if ($_GET['username'] == "admin") {
         exit();
     }
-    $query = $database->exec('DELETE FROM users WHERE username="'.$_POST['username'].'"');
+    $query = $database->exec('DELETE FROM users WHERE username="'.$_GET['username'].'"');
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
