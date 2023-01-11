fc2c97aff45c4c1f6a83126054edc27aefc0e488
panmourovaty <panmourovaty@centrum.cz>
2023-01-11 10:36:23 +0100

    return to original bootstrap

--- craftboard/common.php
+++ craftboard/common.php
@@ -6,7 +6,6 @@ echo '
 <meta content="" name="keywords">
 <meta content="" name="description">
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!-- Favicon -->
 <link href="img/favicon.ico" rel="icon">
 
@@ -27,7 +26,7 @@ echo '
 
 echo '
 <!-- JavaScript Libraries -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="lib/chart/chart.min.js"></script>
 <script src="lib/easing/easing.min.js"></script>
 <script src="lib/waypoints/waypoints.min.js"></script>
