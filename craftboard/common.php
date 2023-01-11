a92990817b0ae1fb82bac35cb5862504f98a8831
panmourovaty <panmourovaty@centrum.cz>
2023-01-11 10:19:48 +0100

    change bootstrap version

--- craftboard/common.php
+++ craftboard/common.php
@@ -26,7 +26,7 @@ echo '
 
 echo '
 <!-- JavaScript Libraries -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="lib/chart/chart.min.js"></script>
 <script src="lib/easing/easing.min.js"></script>
 <script src="lib/waypoints/waypoints.min.js"></script>
