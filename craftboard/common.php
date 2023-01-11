e9bdd5a6acfd2d4606f2561ba585d3c08478a96d
panmourovaty <panmourovaty@centrum.cz>
2023-01-11 10:28:57 +0100

    improve bootstrap loading

--- craftboard/common.php
+++ craftboard/common.php
@@ -6,6 +6,7 @@ echo '
 <meta content="" name="keywords">
 <meta content="" name="description">
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!-- Favicon -->
 <link href="img/favicon.ico" rel="icon">
 
@@ -26,7 +27,7 @@ echo '
 
 echo '
 <!-- JavaScript Libraries -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="lib/chart/chart.min.js"></script>
 <script src="lib/easing/easing.min.js"></script>
 <script src="lib/waypoints/waypoints.min.js"></script>
