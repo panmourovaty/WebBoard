17b5fe926333da8f1acbdfe575a8215ab4d40628
panmourovaty <panmourovaty@centrum.cz>
2023-01-11 08:38:24 +0100

    initial czech lang

---
+++ craftboard/lang/cs_CZ.php
@@ -0,0 +1,22 @@
+<?php
+$lang = array(
+    "langname" => "Čeština",
+    "langcode" => "cs_CZ",
+    "dashboard" => "Hlavní stránka",
+    "servers" => "Servery",
+    "templates" => "Šablony",
+    "backups" => "Zálohy",
+    "settings" => "Nastavení",
+    "createserver" => "Vytvořit Nový Server",
+    "changepassword" => "Změnit Heslo",
+    "oldpassword" => "Staré Heslo",
+    "newpassword" => "Nové Heslo",
+    "change" => "Změnit",
+    "wrongpassword" => "Špatné Heslo",
+    "areyousure" => "Jste si Jistí?",
+    "converttotemplate" => "Konvertovat na Šablonu",
+    "download" => "Stáhnout",
+    "templatename" => "Jméno Šablony",
+    "convert" => "Konvertovat",
+);
+?>
