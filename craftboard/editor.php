7addbc31e3c3bde20ed9069523ec3e85844e8acf
panmourovaty <panmourovaty@centrum.cz>
2023-01-12 23:33:30 +0100

    editor bugfixes

--- craftboard/editor.php
+++ craftboard/editor.php
@@ -2,16 +2,17 @@
 $ACEVERSION = "1.14.0";
 echo'
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu+Mono">
-<div style="position: relative; width: 640px; height: 500px;">
-<div style="position: relative; width: 640px; height: 480px;" id="editor">'.$EDITOR_BASETEXT.'</div>
+<div style="position: relative; width: 100%; height: 90%; z-index:-10;">
+<div style="position: relative; width: 100%; height: 100%;" id="editor">'.$EDITOR_BASETEXT.'</div>
 <br>
-  <select onchange="keybindings()" name="keybindings" id="keybindings">
+<select onchange="keybindings()" name="keybindings" id="keybindings">
     <option value="vscode" selected>VScode</option>
     <option value="vim">VIM</option>
     <option value="emacs">Emacs</option>
 </select>
 <input onchange="fontsize()" type="number" id="fontsize" name="fontsize" min="1" max="100" value="14"></input>
 </div>
+<br>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/'.$ACEVERSION.'/ace.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/'.$ACEVERSION.'/theme-sqlserver.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/'.$ACEVERSION.'/keybinding-vim.min.js" type="text/javascript" charset="utf-8"></script>
