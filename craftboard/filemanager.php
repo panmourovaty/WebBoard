c22e69d6f41c1b6289384c8b4bc892e31bb547a8
panmourovaty <panmourovaty@centrum.cz>
2023-01-28 14:16:49 +0100

    improve icons in file manager

--- craftboard/filemanager.php
+++ craftboard/filemanager.php
@@ -27,7 +27,19 @@ if ($handle = opendir('./files/servers/'.$_GET['server_name'].'/server'.$_GET['f
                 echo '<td><a href="filemanager.php?server_name='.$_GET['server_name'].'&folder_path='.$_GET['folder_path'].'/'.$entry.'"><b><i class="fa-solid fa-folder fa-4x"></i><br>'.$entry.'</b></a></td>';
             }              
             else {
-                echo '<td><a href="#" onClick="MyWindow=window.open(\'filemanager-edit.php?server_name='.$_GET['server_name'].'&folder_path='.$_GET['folder_path'].'&file_name='.$entry.'\',\'MyWindow\',\'width=800,height=800\'); return false;"><i class="fa-solid fa-file fa-4x"></i><br>'.$entry.'</a></td>';
+                switch ($entry) {
+                    case str_ends_with($entry, 'jar'):
+                        echo '<td style="color:#dc3545;"><i class="fa-brands fa-java fa-4x"></i><br>'.$entry;
+                        break;
+                    case str_ends_with($entry, 'json') || str_ends_with($entry, 'yml') || str_ends_with($entry, 'yaml') || str_ends_with($entry, 'properties') || str_ends_with($entry, 'conf') || str_ends_with($entry, 'txt'):
+                        echo '<td><a style="color:#6c757d;" href="#" onClick="MyWindow=window.open(\'filemanager-edit.php?server_name='.$_GET['server_name'].'&folder_path='.$_GET['folder_path'].'&file_name='.$entry.'\',\'MyWindow\',\'width=800,height=800\'); return false;"><i class="fa-solid fa-file-lines fa-4x"></i><br>'.$entry.'</a>';
+                        break;
+                    default:
+                        echo '<td><a style="color:#6c757d;" href="#" onClick="MyWindow=window.open(\'filemanager-edit.php?server_name='.$_GET['server_name'].'&folder_path='.$_GET['folder_path'].'&file_name='.$entry.'\',\'MyWindow\',\'width=800,height=800\'); return false;"><i class="fa-solid fa-file fa-4x"></i><br>'.$entry.'</a>';
+                        break;
+                }
+                echo '<br><a style="color:#198754;" href="#"><i class="fa-solid fa-file-arrow-down"></i></a>&nbsp;';
+                echo '<a style="color:#dc3545;" href="#"><i class="fa-solid fa-file-circle-xmark"></i></a></td>';
             }
             if ($n == $m) {
                 echo '</tr>';
